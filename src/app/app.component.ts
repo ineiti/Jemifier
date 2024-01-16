6ce7f469f2bdd6ec5c8d74eab5aee7d2ad8aaa0e
Linus Gasser <linus.gasser@epfl.ch>
2024-01-16 22:07:05 +0100

    Async injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-// import { DataComponent } from './data.component';
-// import { parse } from "papaparse";
-import { parse } from "csv-parse";
+import { DataComponent } from './data.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  providers: [DataComponent],
   imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
@@ -15,12 +14,12 @@ import { parse } from "csv-parse";
 export class AppComponent {
   title = 'Jemifier';
 
-  // constructor(private data_component: DataComponent){}
+  constructor(private data_component: DataComponent) { }
 
-  ngOnInit() {
-    // const b = new Buffer(12);
-    const b = parse("123,456");
-    console.dir(b);
-    // console.log(`Got data: {data_component.list_books}`);
+  async ngOnInit() {
+    // // const b = new Buffer(12);
+    // const b = parse("123,456");
+    // console.dir(b);
+    console.log(`Got data:`, await this.data_component.list_books());
   }
 }
