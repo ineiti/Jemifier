658251444a5e3a47215ae6aad82649aad1cd6710
Linus Gasser <linus.gasser@epfl.ch>
2024-03-24 18:23:44 +0100

    Added updating command to devbox

--- src/app/dates/dates.component.ts
+++ src/app/dates/dates.component.ts
@@ -8,23 +8,25 @@ import { Router, RouterLink } from '@angular/router';
 import { SongListComponent } from "../song-list/song-list.component";
 
 @Component({
-    selector: 'app-dates',
-    standalone: true,
-    templateUrl: './dates.component.html',
-    styleUrl: './dates.component.scss',
-    imports: [CommonModule, RouterLink, SongListComponent]
+  selector: 'app-dates',
+  standalone: true,
+  templateUrl: './dates.component.html',
+  styleUrl: './dates.component.scss',
+  imports: [CommonModule, RouterLink, SongListComponent]
 })
 export class DatesComponent {
   @Input() date?: string;
-  list_services?: ListServices;
   list_songs?: ListSongs;
+  services?: Service[];
   constructor(private data_component: DataService, private router: Router) { }
 
-  async ngOnChanges(){
-    this.list_songs = await this.data_component.list_songs();
-    this.list_services = await this.data_component.list_services();
-
+  async ngOnChanges() {
     document.title = "Jemifier - dates";
+    if (this.list_songs === undefined || this.services === undefined) {
+      this.list_songs = await this.data_component.list_songs();
+      this.services = (await this.data_component.list_services()).services.slice().reverse().slice(0);
+    }
+
     if (this.date !== undefined) {
       document.title = `Jemifier - ${this.date}`;
       setTimeout(() => {
--- utils/create_songs.ts
+++ utils/create_songs.ts
@@ -62,7 +62,7 @@ async function readSongs(bookNbr: number, bookAbb: string, songPath: string): Pr
             const [_, songNbrStr, title] = song.song.title[0].match(regexp);
             const songNbr = parseInt(songNbrStr);
             // console.log(`${fullTitle}:: ${songNbr} - ${title}`);
-            songs.push(new Song({ book_id: bookNbr, number: songNbr, title: title, lyrics: song.song.lyrics[0] }));
+            songs.push(new Song(-1, { book_id: bookNbr, number: songNbr, title: title, lyrics: song.song.lyrics[0] }));
             // break;
         } catch (e) {
             console.log(`While parsing ${fullTitle}, got error ${e}`);
